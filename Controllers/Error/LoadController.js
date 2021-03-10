0b535ab7d906952a4a92c393fa62feb63f849883
Sam Oyekeye <sam@Sams-MacBook-Pro.local>
2021-03-10 08:58:44 +0100

    added log

--- Controllers/Error/LoadController.js
+++ Controllers/Error/LoadController.js
@@ -70,6 +70,7 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    console.log(err);
     let error = { ...err };
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(
 );
 
 // Serving static files
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/Public`));
 
 app.use(compression());
 
